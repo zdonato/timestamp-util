7b37e9e691aa6a3706785893db374849d074b5da
Zachary Donato <zdonato@stevens.edu>
2016-06-15 16:00:32 -0400

    Switched return to console.log

--- timestamp-util.js
+++ timestamp-util.js
@@ -1,7 +1,7 @@
 var moment = require('moment');
 
 var timestamp = function (msg) {
-    return '[' + moment().format('MMMM Do YYYY, h:mm:ss a') + ']: ' + msg;
+    console.log('[' + moment().format('MMMM Do YYYY, h:mm:ss a') + ']: ' + msg);
 };
 
 module.exports = timestamp;
